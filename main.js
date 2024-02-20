5b4b981e01bdec39186cc063f824a19ba285c49d
joss_vaz <vjoseline255@gmail.com>
2024-02-20 14:53:09 -0600

    Actualice mi presentación

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let typewriter = new Typewriter(app, {
  
 typewriter
   .pauseFor(2500)
-  .typeString(' <span style ="color:#481800"> Desarrollo sitios web y programas aplicados en biotecnología.<\span>')
+  .typeString(' <span style ="color:#481800"> Soy biotecnóloga, realizo investigación, desarrollo programas aplicados en biotecnología y sitios web.<\span>')
   .pauseFor(200)
   .deleteChars(10)
   .start();
